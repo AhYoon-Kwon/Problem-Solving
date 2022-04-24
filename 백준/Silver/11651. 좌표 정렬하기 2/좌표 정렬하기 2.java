c97f88f1365e1bafb97950566825436bc0a9e332
AhYoon-Kwon <99807551+AhYoon-Kwon@users.noreply.github.com>
2022-04-24 21:11:02 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2860 ms, Memory: 203300 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[][] dot = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			dot[i][0] = scan.nextInt();
+			dot[i][1] = scan.nextInt();
+		}
+		
+		Arrays.sort(dot, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				//y좌표가 같으면 x좌표가 증가하는 순
+				if(o1[1] == o2[1])
+					return o1[0] - o2[0];
+				//그외 기본은 y좌표가 증가하는 순
+				else
+					return o1[1] - o2[1];
+			}
+		});
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.print(dot[i][j]+ " ");
+			}
+			System.out.println();
+		}
+		
+		scan.close();
+
+	}
+
+}
