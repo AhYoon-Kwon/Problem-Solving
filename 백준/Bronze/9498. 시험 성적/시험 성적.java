c8e9a7b13710f5abe26ddfc40eb18d92c85de546
AhYoon-Kwon <99807551+AhYoon-Kwon@users.noreply.github.com>
2024-07-03 22:34:51 +0900

    [Bronze V] Title: 시험 성적, Time: 96 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String []args) {
-        Scanner scan = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int score = scan.nextInt();
+        switch (score / 10) {
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
         scan.close();
-        
-        if(score>=90 && score <=100) System.out.println("A");
-        else if (score>=80) System.out.println("B");
-        else if (score>=70) System.out.println("C");
-        else if (score>=60) System.out.println("D");
-        else System.out.println("F");
     }
 }
